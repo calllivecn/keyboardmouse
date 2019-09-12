2c4b014e391c07a71d096ed7c04a5cd86989c760
calllivecn <calllivecn@outlook.com>
2019-09-12 15:52:54 +0800

    调好 setup。py save 修改：     .gitignore 重命名：   libhotkye.py -> libhotkey.py 修改：     setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 # date 2019-09-11 20:30:10
 # author calllivecn <c-all@qq.com>
 
-#from setuptools import setup
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 
 setup(
         name="keyboardmouse",
@@ -13,8 +13,9 @@ setup(
         author="calllivecn",
         author_email="c-all@qq.com",
         url="https://github.com/calllivecn/keyboardmouse",
-        py_modules=["libkbm","libhotkey"],
-        requires=["libevdev>=0.7"],
+        py_modules=["libkbm","libhotkey", "logs"],
+        #packages=["libkbm"],
+        install_requires=["libevdev>=0.7"],
         platforms=["linux"],
         scripts=["list-inputs.py", "checkkey.py"]
     )
