c21f4b1c0d226f13b60b08139008321e614525aa
calllivecn <c-all@qq.com>
2021-10-23 17:17:26 +0800

    fix mouse.py 发送错误键名

---
+++ mediakey.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2021-10-23 13:27:29
+# author calllivecn <c-all@qq.com>
+
+# 某体键：音量调节
+# type 01 EV_KEY code 073 KEY_VOLUMEUP
+# type 01 EV_KEY code 072 KEY_VOLUMEDOWN
+
+
