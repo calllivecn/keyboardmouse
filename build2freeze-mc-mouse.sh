7428e7167fd96d13a454b42a5d936e91bfb5dec5
calllivecn <calllivecn@outlook.com>
2023-06-26 04:51:48 +0800

    update

--- build2freeze-mc-mouse.sh
+++ build2freeze-mc-mouse.sh
@@ -20,7 +20,13 @@ set -e
 trap "safe_exit" SIGTERM SIGINT EXIT
 
 cp -rv keyboardmouse/ "$TMP/"
-cp -v mouse.py "$TMP/__main__.py"
+
+rm -rvf keyboardmouse/__pycache__/
+
+cat >"$TMP/__main__.py"<<EOF
+from keyboardmouse import mouse
+mouse.main()
+EOF
 
 python3 -m zipapp "$TMP" -c -o mouse.pyz -p "/usr/bin/env python3"
 
--- mouse.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# coding=utf-8
-# date 2023-06-26 04:01:14
-# author calllivecn <c-all@qq.com>
-
-from keyboardmouse import mouse
-
-
-mouse.main()
